ffbee6343f24c111ab3c35a8e86f2dbe0d55a94a
CelinaPlatt <alvarezdiaz.celina@gmail.com>
2021-10-19 15:41:37 +0100

    Add BrowerRouter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Header from './components/Header';
 import ReviewList from './components/ReviewList';
 import Profile from './components/Profile';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   const [user, setUser] = useState({
@@ -13,14 +14,39 @@ function App() {
       'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg',
   });
 
-
   return (
-    <div className="App">
+    <BrowserRouter>
       <Nav avatar={user.avatar_url} username={user.username} />
-      <Header/>
-      <ReviewList avatar={user.avatar_url} username={user.username} />
-      <Profile user={user}/>
-    </div>
+      <Switch>
+        <Route exact path="/">
+          {/*      CategoryMenu */}
+          {/*      HomeGallery */}
+        </Route>
+        <Route exact path="/profile">
+          <Profile user={user} />
+          <ReviewList avatar={user.avatar_url} username={user.username} />
+        </Route>
+        <Route exact path="/login">
+          {/* Login  loginInputs ={inputs}*/}
+        </Route>
+        <Route exact path="/register">
+          {/* Login  registerInputs ={inputs}*/}
+        </Route>
+        <Route exact path="/profile/editor">
+          {/* Editor  formInputs ={inputs}*/}
+        </Route>
+        <Route exact path="reviews/:review_id">
+          {/* Editor  formInputs ={inputs}*/}
+        </Route>
+        <Route exact path="reviews/:review_id/editor">
+          {/* Review */}
+        </Route>
+        <Route exact path="reviews/:category_id">
+          <Header />
+          <ReviewList avatar={user.avatar_url} username={user.username} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
