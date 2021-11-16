7912f37a355481eed035113a59c505773715f481
CelinaPlatt <alvarezdiaz.celina@gmail.com>
2021-11-16 07:01:16 +0000

    Add Material UI theme

--- src/App.js
+++ src/App.js
@@ -12,65 +12,81 @@ import HeaderFullPageReview from './components/HeaderFullPageReview';
 import Login from './components/Login';
 import Register from './components/Register';
 import { UserProvider } from './contexts/User';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#BC7D34',
+      contrastText:'black',
+    },
+    secondary: {
+      main: '#52282f',
+    },
+  },
+});
 
 function App() {
   return (
-    <UserProvider>
-      <UsersProvider>
-        <BrowserRouter>
-          <Nav />
-          <Switch>
-            <Route exact path="/">
-              <CategoryMenu />
-              {/*      HomeGallery */}
-            </Route>
+    <ThemeProvider theme={theme}>
+      <UserProvider>
+        <UsersProvider>
+          <BrowserRouter>
+            <Nav />
+            <Switch>
+              <Route exact path="/">
+                <CategoryMenu />
+                {/*      HomeGallery */}
+              </Route>
 
-            <Route exact path="/:username/profile">
-              <Profile />
-              <ReviewList />
-            </Route>
+              <Route exact path="/:username/profile">
+                <Profile />
+                <ReviewList />
+              </Route>
 
-            <Route exact path="/login">
-              <Login />
-            </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
 
-            <Route exact path="/register">
-              <Register />
-            </Route>
+              <Route exact path="/register">
+                <Register />
+              </Route>
 
-            <Route exact path="/profile/editor">
-              {/* Editor  formInputs ={inputs}*/}
-            </Route>
+              <Route exact path="/profile/editor">
+                {/* Editor  formInputs ={inputs}*/}
+              </Route>
 
-            <Route exact path="/reviews/:review_id">
-              <HeaderFullPageReview />
-              <ReviewList />
-            </Route>
+              <Route exact path="/reviews/:review_id">
+                <HeaderFullPageReview />
+                <ReviewList />
+              </Route>
 
-            <Route exact path="/:category/reviews">
-              <HeaderCategory />
-              <ReviewList />
-            </Route>
+              <Route exact path="/:category/reviews">
+                <HeaderCategory />
+                <ReviewList />
+              </Route>
 
-            <Route exact path="/users/:username/reviews">
-              <HeaderUserReviews />
-              <ReviewList />
-            </Route>
+              <Route exact path="/users/:username/reviews">
+                <HeaderUserReviews />
+                <ReviewList />
+              </Route>
 
-            <Route exact path="editor/reviews/:review_id">
-              {/* Review */}
-            </Route>
-            <Route exact path="/:notaccountedfor">
-              <img
-                className="errGif"
-                src="/images/404-error.gif"
-                alt="Not Found"
-              />
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </UsersProvider>
-    </UserProvider>
+              <Route exact path="editor/reviews/:review_id">
+                {/* Review */}
+              </Route>
+              <Route exact path="/:notaccountedfor">
+                <img
+                  className="errGif"
+                  src="/images/404-error.gif"
+                  alt="Not Found"
+                />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </UsersProvider>
+      </UserProvider>
+      </ThemeProvider>
   );
 }
 
